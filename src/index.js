8d996dba8dea08b78d0d000dfb641b2d52b20dc7
cweihan92 <43396023+cweihan92@users.noreply.github.com>
2018-09-25 13:13:29 +0800

    step1: add TodoApp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import TodoApp from './step1/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
 registerServiceWorker();
