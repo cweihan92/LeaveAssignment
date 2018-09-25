b8186973b816ce7de78c89f5c4f298c3cf8ba236
cweihan92 <43396023+cweihan92@users.noreply.github.com>
2018-09-25 13:36:33 +0800

    Step2: ajax call from another URL in lifecycle componentDidMount

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoApp from './step1/App';
+import App from './step2/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
